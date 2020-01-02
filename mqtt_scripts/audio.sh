8a999958c964057fc44b3a18810d6a0747ae4c63
sergge1 <shvozdyk@gmail.com>
2020-01-02 15:45:09 +0200

    mqtt scripts added

---
+++ mqtt_scripts/audio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# chmod +x start_py.sh
+# ./start_py.sh
+
+cd ~/mqtt_clients/test
+sudo python audio.py
+
---
+++ mqtt_scripts/led.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# chmod +x start_py.sh
+# ./start_py.sh
+
+cd ~/mqtt_clients
+sudo python led.py
+
